16e8556efa75c1a255de21406ebb10d359f98679
Marcus Bello <marcusthetechguru@gmail.com>
2023-06-23 11:20:15 -0400

    wip

---
+++ crm.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"github.com/marcusbello/go-crm/internal/server"
+	"github.com/marcusbello/go-crm/internal/server/storage/mem"
+	"log"
+)
+
+// General service flags.
+var (
+	addr = flag.String("addr", "0.0.0.0:6742", "The address to run the service on.")
+)
+
+func main() {
+	flag.Parse()
+
+	// Setup for the service.
+	store := mem.New()
+
+	s, err := server.New(
+		*addr,
+		store,
+		server.WithGRPCOpts(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	done := make(chan error, 1)
+
+	log.Println("Starting server at: ", *addr)
+	go func() {
+		defer close(done)
+		done <- s.Start()
+	}()
+
+	log.Println("Server exited with error: ", <-done)
+}
